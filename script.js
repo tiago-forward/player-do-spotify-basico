6956f80c06a7572077e09d4bcb64d2731288c9e5
tiagoforward <devtiagoforward@gmail.com>
2024-04-01 19:01:35 -0300

    adding timer to music

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
 const currentProgress = document.getElementById('current-progress')
-
-const asSmart = {
-    songName: 'Smart',
-    artist: 'Lee Serafim',
-    file: 'LE SSERAFIM Smart'
-};
-
-const asManiac = {
-    songName: 'Maniac',
-    artist: 'VIVIZ',
-    file: 'VIVIZ MANIAC'
-};
-
-const asDrama = {
-    songName: 'Drama',
-    artist: 'Aespa',
-    file: 'aespa Drama'
-};
-
-const asBornToBe = {
-    songName: 'Born to be',
-    artist: 'ITZY',
-    file: 'ITZY BORN TO BE'
-};
+const numberProgressStart = document.getElementById('number-progress-start')
+let numberProgressEnd = document.getElementById('number-progress-end')
 
 let isPlaying = false;
-const playList = [asSmart, asManiac, asDrama, asBornToBe];
+
+const playList = [{
+    songName: 'Smart',
+    artist: 'Lee Serafim',
+    file: 'LE SSERAFIM Smart',
+    duration: '-02:46'
+}, {
+    songName: 'Maniac',
+    artist: 'VIVIZ',
+    file: 'VIVIZ MANIAC',
+    duration: '-03:15'
+}, {
+    songName: 'Drama',
+    artist: 'Aespa',
+    file: 'aespa Drama',
+    duration: '-03:35'
+}, {
+    songName: 'Born to be',
+    artist: 'ITZY',
+    file: 'ITZY BORN TO BE',
+    duration: '-02:58'
+}]
+
 let index = 0;
 
 function playSong() {
@@ -62,6 +62,8 @@ function initializeSong(){
     song.src = `songs/${playList[index].file}.mp3`
     songName.innerText = playList[index].songName
     bandName.innerText = playList[index].artist
+    numberProgressStart.innerText = '00:00'
+    numberProgressEnd.innerText = playList[index].duration
 };
 
 function previousSong(){
@@ -85,11 +87,22 @@ function nextSong(){
 };
 
 function updateProgressBar(){
-    // song.currentTime //tempo atual da música em segundos
-    // song.duration //duração total da música em segundos
+    function readableDuration(seconds) {
+        sec = Math.floor( seconds );    
+        min = Math.floor( sec / 60 );
+        min = min >= 10 ? min : '0' + min;    
+        sec = Math.floor( sec % 60 );
+        sec = sec >= 10 ? sec : '0' + sec;
+        return `${min}:${sec}`
+    }
+    
     const barWidth = (song.currentTime / song.duration)*100
     currentProgress.style.setProperty('--progress', `${barWidth}%`) 
-};
+    
+    numberProgressStart.innerText = readableDuration(song.currentTime);
+    let numberProgress = song.duration - song.currentTime
+    numberProgressEnd.innerText = `-${readableDuration(numberProgress)}`;
+}
 
 initializeSong();
 
